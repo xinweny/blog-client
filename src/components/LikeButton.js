2f67db11fa1daf69215cf6414a2082dd3fc8276b
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-17 19:22:02 -0400

    disable button when no user is logged in

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import { PropTypes as PT } from 'prop-types';
 
-function LikeButton() {
+import { useFetch } from '../utils/hooks';
+import { sendReq } from '../utils/helpers';
+
+function LikeButton({ postId }) {
+  const [likesCount, setLikesCount] = useFetch(`posts/${postId}/likes?postId=${postId}`);
+
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleClick = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const res = await sendReq('POST', `posts/${postId}/likes`, { postId }, token);
+
+      if (res.status === 200) {
+        setLikesCount(prev => prev + 1);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div></div>
+    <div>
+      <button onClick={handleClick} disabled={!!user}>
+        <img src="#" alt="Like" />
+      </button>
+      <p>{likesCount}</p>
+    </div>
   );
+}
+
+LikeButton.propTypes = {
+  postId: PT.string.isRequired,
 }
 
 export default LikeButton;
