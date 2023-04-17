bced6ff3bbb718f2431573fca8397bb060c873ee
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-17 17:28:30 -0400

    refactor fetch from api

--- src/components/PostsOverview.js
+++ src/components/PostsOverview.js
@@ -5,13 +5,13 @@ import { useFetch } from '../utils/hooks';
 import PostCardMini from './PostCardMini';
 
 function PostsOverview() {
-  const data = useFetch('https://blog-api-5lv9.onrender.com/api/posts?published=true&likesCount=desc&limit=10');
+  const posts = useFetch('posts?published=true&likesCount=desc&limit=10');
   
   return (
     <div>
       <h3>Recent Stories</h3>
-      {data 
-        ? data.posts.map((post) => (
+      {posts 
+        ? posts.map((post) => (
           <PostCardMini key={post._id} post={post} />
         )) : null}
     </div>
