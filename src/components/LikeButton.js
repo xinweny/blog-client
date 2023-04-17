bd03a5964a9ae5ffc10fe2916683b6b01ece931c
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-17 17:28:20 -0400

    refactor fetching data from api

---
+++ src/components/LikeButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LikeButton() {
+  return (
+    <div></div>
+  );
+}
+
+export default LikeButton;
--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -5,14 +5,14 @@ import { useFetch } from '../utils/hooks';
 import PostCardMini from './PostCardMini';
 
 function TopStories() {
-  const data = useFetch('https://blog-api-5lv9.onrender.com/api/posts?published=true&likesCount=desc&limit=10');
+  const posts = useFetch('posts?published=true&likesCount=desc&limit=10');
 
   return (
     <div>
       <h3>Trending</h3>
       <div>
-        {data
-          ? data.posts.map(post => (
+        {posts
+          ? posts.map(post => (
             <PostCardMini key={post._id} post={post} />
           )) : null}
       </div>
