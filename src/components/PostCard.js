f6f5334c0901a51338f85332e92ebbb8ddc5be79
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-04-21 13:15:04 -0400

    add user profile link

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -11,7 +11,9 @@ function PostCard({ post }) {
       <Link to={`/posts/${post._id}`}>
         <h4>{post.title}</h4>
       </Link>
-      <p>{post.author.username}</p>
+      <Link to={`/users/${post.author._id}`}>
+        <p>{post.author.username}</p>
+      </Link>
       <p>{format(parseISO(post.createdAt), 'd MMM Y')}</p>
       <div>
         {uniqueTags.map(tag => <p key={tag}>{tag}</p>)}
@@ -27,13 +29,10 @@ PostCard.propTypes = {
     _id: PT.string.isRequired,
     title: PT.string.isRequired,
     createdAt: PT.string.isRequired,
-    author: PT.oneOfType(
-      PT.string.isRequired,
-      PT.shape({
-        _id: PT.string.isRequired,
-        username: PT.string.isRequired,
-      }),
-    ),
+    author: PT.shape({
+      _id: PT.string.isRequired,
+      username: PT.string.isRequired,
+    }),
     likesCount: PT.number.isRequired,
     commentsCount: PT.number.isRequired,
     tags: PT.arrayOf(PT.string),
